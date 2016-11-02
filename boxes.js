6318d54d86cc5b7638e68ffcd235d5b4492600b1
Chris Houdlette <choudlette@gmail.com>
2016-11-01 20:55:01 -0600

    progress made

--- boxes.js
+++ boxes.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+$('document').ready(function() {
+  alert('Ready for DOM Manipulation');
+  $('#secretBox').css('background', 'white');
+  $('#secretBox').append("<h1>This is the Secret Box</h1>");
+  $('#row1 > .box').addClass('boxType3');
+  $('#row3 >.box:last-child').hide();
+  $('.boxType1').css("background-color", 'white');
+  $('#row2 > .box:first-child')
+
+});
